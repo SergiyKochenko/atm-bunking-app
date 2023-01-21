c6c8caf74ffb5c9d35d9a2fea2c75e531bb4a1e2
SergiyKochenko <sergiykochenko@gmail.com>
2023-01-21 20:10:20 +0000

    bugs fixing

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 import gspread
-from google.oauth2.service_account import Credentials
 
 from cardHolder import cardHolder
+
+from google.oauth2.service_account import Credentials
+
+
 
 
 SCOPE = [
