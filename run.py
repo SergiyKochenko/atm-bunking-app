da0e66f4d0d3d413b5966e1c8c67cd120466b983
SergiyKochenko <sergiykochenko@gmail.com>
2023-01-24 12:56:30 +0000

    add link to readme to deployed site

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 import gspread
 from cardHolder import cardHolder
 from google.oauth2.service_account import Credentials
-
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -33,44 +32,47 @@ logo = (
        AA   AA   TT    MM MM MM
       AAAAAAAAA  TT    MM    MM
      AAA     AAA TT    MM    MM
-      """  
+      """
   )
 print(logo)
 
+
 def print_menu():
-   # Print options to the user
-   print("Please chose from one of the follewing options...")
-   print("1. Deposit")
-   print("2. Withdraw")
-   print("3. Show Balance")
-   print("4. Exit")
+    # Print options to the user
+    print("Please chose from one of the follewing options...")
+    print("1. Deposit")
+    print("2. Withdraw")
+    print("3. Show Balance")
+    print("4. Exit")
+
 
 def deposit(cardHolder):
   try:
     deposit = float(input("How much € would you like to deposit: "))
     cardHolder.set_balance(cardHolder.get_balance() + deposit)
     print("Thank you for your deposit. Your new balance is: €", str(cardHolder.get_balance()))
-  except:
-    print("Invalid input")
+  except ValueError():
+      print("Invalid input")
+
 
 def withdraw(cardHolder):
   try:
     withdraw = float(input("How much € would you like to withdraw: "))
     # Chkesk if user has enough mony
-    if(cardHolder.get_balance() < withdraw):
+    if (cardHolder.get_balance() < withdraw):
       print("Insufficient balance :(")
     else:
       cardHolder.set_balance(cardHolder.get_balance() - withdraw)
       print("You're good to go! Thank you :)")
-  except:
+  except ValueError():
     print("Invalit input")
+
 
 def check_balance(cardHolder):
   print("Your current balance is: €", cardHolder.get_balance())
 
 if __name__ == "__main__":
-  current_user = cardHolder("","","","","")
-
+  current_user = cardHolder("", "", "", "", "")
   # Create a repo of cardholders
   list_of_cardHolders = []
   list_of_cardHolders.append(cardHolder("4532772818527395", 1234, "John", "Griffin", 1050.30))
@@ -78,20 +80,19 @@ if __name__ == "__main__":
   list_of_cardHolders.append(cardHolder("5128381368581872", 6543, "Flavia", "Jeckson", 150.79))
   list_of_cardHolders.append(cardHolder("6011188364697109", 8765, "Kira", "Dopkins", 950.93))
   list_of_cardHolders.append(cardHolder("3490693153147110", 2040, "Anna", "Watson", 10.28))
-  
   # Prompt user for debit card number
   debitCardNum = ""
   while True:
     try:
       debitCardNum = input("\nPlease insert your debit card: ")
-      #Check against repo
+      # Check against repo
       debitMatch = [holder for holder in list_of_cardHolders if holder.cardNum == debitCardNum]
-      if(len(debitMatch) > 0):
-        current_user =debitMatch[0]
+      if (len(debitMatch) > 0):
+        current_user = debitMatch[0]
         break
       else:
         print("Card number not recognized. Please try again.")
-    except:
+    except ValueError():
       print("Card number not recognized. Please try again.")
 
 # Prompt for PIN
@@ -99,14 +100,14 @@ is_on = True
 while is_on:
   try:
     userPin = int(input("\nPlease enter your pin: ").strip())
-    if(current_user.get_pin() == userPin):
+    if (current_user.get_pin() == userPin):
       is_on = False
     else:
       print("Invalid PIN. Please try again.")
       break
-  except:
+  except ValueError():
     print("Invalid PIN. Please try again.")
-
+  
   # Print options
   print("\nWelcome ", current_user.get_firstName(), " :)")
   option = 0
@@ -117,15 +118,14 @@ while is_on:
     except:
       print("Invalid input. Please try again.")
 
-    if(option == 1):
+    if (option == 1):
       deposit(current_user)
-    elif(option == 2):
+    elif (option == 2):
       withdraw(current_user)
-    elif(option == 3):
+    elif (option == 3):
       check_balance(current_user)
-    elif(option == 4):
+    elif (option == 4):
       break
     else:
       option = 0
-
-  print("\nThank you. Have a nice day :)")
+print("\nThank you. Have a nice day :)")
